a2dee0dec8c016a87b84b09fbe520858ef88f614
djordon <daniel@seatgeek.com>
2017-06-05 02:01:51 -0400

    Added methods for plotting polygons, rectangles and extended corners

--- src/main/scala/org/partitioner/plot.scala
+++ src/main/scala/org/partitioner/plot.scala
@@ -3,7 +3,8 @@ package org.partitioner
 import java.io.File
 
 import com.vividsolutions.jts.geom.Polygon
-import plotly.element.{Color, Line, Marker, ScatterMode, Fill}
+
+import plotly.element.{Color, Line, Marker, ScatterMode}
 import plotly.layout.{Axis, Layout}
 import plotly.{Plotly, Scatter}
 
@@ -21,68 +22,51 @@ object PolygonPlotter {
     )
   }
 
-  def extendedCornerPlotter(line: ExtendedCorner): List[Scatter] = {
+  def extendedCornerPlotter(line: ExtendedCorner): Scatter = {
     val points: List[Point] = List(line.source, line.dest)
 
-    List(scatterLines(
+    scatterLines(
       points=points,
       markerColor=Color.RGBA(152, 0, 0, 0.8),
       markerLine=Line(color = Color.RGBA(30, 0, 0, 1.0), width = 1.0)
-    ))
+    )
   }
 
-  def rectanglePlotter(rectangle: Rectangle): List[Scatter] = {
+  def rectanglePlotter(rectangle: Rectangle): Scatter = {
+    val points: List[Point] = List(rectangle.upperLeft, rectangle.lowerRight)
 
-    val top: List[Point] = List(rectangle.upperLeft, rectangle.upperRight)
-    val bot: List[Point] = List(rectangle.lowerLeft, rectangle.lowerRight)
-
-    val blueShade: Int = (Math.random() * 200).toInt + 55
-    val blue: Color = Color.RGBA(0, 15, blueShade, 0.7)
-    val marker: Marker = Marker(color = blue, line = Line(color = blue, width = 1.0))
-
-    Scatter(
-      values = bot.map(_.x),
-      secondValues = bot.map(_.y),
-      mode = ScatterMode(ScatterMode.Markers, ScatterMode.Lines),
-      marker = marker
-    ) ::
-    Scatter(
-      values = top.map(_.x),
-      secondValues = top.map(_.y),
-      mode = ScatterMode(ScatterMode.Markers, ScatterMode.Lines),
-      marker = marker,
-      fill = Fill.ToNextY
-    ) :: Nil
+    scatterLines(
+      points=points,
+      markerColor=Color.RGBA(0, 152, 0, 0.8),
+      markerLine=Line(color = Color.RGBA(0, 30, 0, 1.0), width = 1.0)
+    )
   }
 
-  def polygonPlotter(polygon: Polygon): List[Scatter] = {
-    val interior: List[List[Point]] = polygon.getHoles.map(_.toList.map(Point.apply))
-    val exterior: List[Point] = polygon.toList.map(Point.apply)
+  def polygonPlotter(polygon: Polygon): Scatter = {
+    val points: List[Point] = polygon.toList.map(p => Point(p.x, p.y))
 
-    val scatterPartial = scatterLines(
-      _: List[Point],
+    scatterLines(
+      points=points,
       markerColor=Color.RGBA(255, 153, 51, 0.8),
       markerLine=Line(color = Color.RGBA(255, 153, 51, 0.8), width = 1.0)
     )
-
-    (exterior :: interior).map(scatterPartial)
   }
 
   def quickPlot(
-      polygons: List[Polygon],
+      polygon: Polygon,
       innerLines: List[ExtendedCorner] = Nil,
       diagLines: List[Rectangle] = Nil,
       plotName: String = "quick",
       fileName: String = "quick.html"): File = {
 
-    val scatters: List[Scatter] = polygons.flatMap(polygonPlotter) ++
-      innerLines.flatMap(extendedCornerPlotter) ++
-      diagLines.flatMap(rectanglePlotter)
+    val scatters: Seq[Scatter] = Seq(polygonPlotter(polygon)) ++
+      innerLines.map(extendedCornerPlotter) ++
+      diagLines.map(rectanglePlotter)
 
     val layout = Layout(
       title = plotName,
-      xaxis = Axis(showgrid=true),
-      yaxis = Axis(showgrid=true),
+      xaxis = Axis(showgrid=false),
+      yaxis = Axis(showgrid=false),
       width = 600,
       height = 600
     )
