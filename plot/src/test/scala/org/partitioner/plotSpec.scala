9513df4271010c62eec369efc3ae62f7c957e413
Daniel <dan.jordon@gmail.com>
2017-06-18 13:18:45 -0400

    Relocated plot code into its own submodule. Added an empty test

---
+++ plot/src/test/scala/org/partitioner/plotSpec.scala
@@ -0,0 +1,16 @@
+package org.partitioner
+
+import org.scalatest.{Matchers, WordSpec}
+
+import scala.language.reflectiveCalls
+
+
+
+class PolygonPlotterSpec extends WordSpec with Matchers with PolygonFixtures {
+  "PolygonPlotter" can {
+    "quickPlot" should {
+      "do something" in {
+      }
+    }
+  }
+}
