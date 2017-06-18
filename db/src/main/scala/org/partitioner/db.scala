89c30b0cd1a5d9c157003c4efe130f1c56b073d3
Daniel <dan.jordon@gmail.com>
2017-06-18 13:19:47 -0400

    Added code to access a postgis db with polygons in its own submodule

---
+++ db/src/main/scala/org/partitioner/db.scala
@@ -0,0 +1,53 @@
+package org.partitioner.db
+
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import com.vividsolutions.jts.io.WKTReader
+import com.vividsolutions.jts.geom.MultiPolygon
+
+import doobie.imports._
+import scalaz.concurrent.Task
+
+
+case class MultiPolygonZip(zipcode: String, multipolygon: MultiPolygon) {}
+
+
+object PolygonExtractor {
+
+  object MultiPolygonZip {
+    def apply(zc: String, wkt: String) = {
+      val wktReader = new WKTReader()
+      new MultiPolygonZip(zc, wktReader.read(wkt).asInstanceOf[MultiPolygon])
+    }
+  }
+
+  def extractZCTAs(numPolygons: Int = 1): List[MultiPolygonZip] = {
+    val sysUser = sys.env.get("USER").get
+    val hostname = sys.env.getOrElse("POSTGIS_HOST", "localhost")
+    val username = sys.env.getOrElse("POSTGIS_USERNAME", sysUser)
+    val password = sys.env.getOrElse("POSTGIS_PASSWORD", "")
+    val database = sys.env.getOrElse("POSTGIS_DATABASE", "")
+    val schema = sys.env.getOrElse("POSTGIS_SCHEMA", "public")
+    val post = sys.env.getOrElse("POSTGIS_SCHEMA", 5432)
+
+    val xa = DriverManagerTransactor[Task](
+      "org.postgresql.Driver",
+      s"jdbc:postgresql:$database",
+      username,
+      password
+    )
+
+    sql"""
+      SELECT
+        geoid10  -- zip code
+        , ST_AsText(geom)  -- multi polygon
+      FROM tiger.zcta
+      ORDER BY geoid10
+      LIMIT $numPolygons;"""
+      .query[(String,  String)]
+      .list
+      .transact(xa)
+      .unsafePerformSync
+      .map(Function.tupled(MultiPolygonZip.apply _))
+  }
+}
