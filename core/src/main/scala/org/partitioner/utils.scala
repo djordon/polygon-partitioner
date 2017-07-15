160e9da743923bd21939e5960a8927e97fe5f335
djordon <daniel@seatgeek.com>
2017-07-15 17:51:57 -0400

    Moved isOrthogonalPolygon to GeometryUtils and added documentation. Removed createPolygon function

--- core/src/main/scala/org/partitioner/utils.scala
+++ core/src/main/scala/org/partitioner/utils.scala
@@ -10,7 +10,6 @@ object GeometryUtils {
   lazy val geometryFactory = new GeometryFactory()
   lazy val wktReader = new WKTReader()
 
-
   implicit class IterablePolygon(val pg: Polygon) extends Iterable[Coordinate] {
     override def iterator: Iterator[Coordinate] = pg.getExteriorRing.getCoordinates.toIterator
 
@@ -18,7 +17,7 @@ object GeometryUtils {
       val holes: IndexedSeq[LineString] = for {i <- 0 until pg.getNumInteriorRing }
         yield pg.getInteriorRingN(i)
 
-      holes.map(ls => createPolygon(ls.getCoordinates)).toList
+      holes.map(ls => geometryFactory.createPolygon(ls.getCoordinates)).toList
     }
 
     def getHolesCoordinates: List[List[Coordinate]] = {
@@ -31,10 +30,19 @@ object GeometryUtils {
 
   def normalizePolygon(pg: Polygon): Polygon = pg.norm.asInstanceOf[Polygon]
 
-  def createPolygon(coordinates: Array[Coordinate]): Polygon = {
-    val polygon = geometryFactory.createPolygon(coordinates)
-    polygon.normalize()
+  /**
+   * Returns a boolean the input polygon is an orthogonal polygon
+   *
+   * @param polygon the input polygon
+   *
+   * @return A boolean indicating whether the input is an orthogonal polygon
+   */
+  def isOrthogonalPolygon(polygon: Polygon): Boolean = {
     polygon
+      .toList
+      .sliding(2, 1)
+      .collect { case a :: b :: Nil => a.x == b.x || a.y == b.y }
+      .reduce(_ && _)
   }
 
   def loadResources(dir: String) : Map[String, Polygon] = Source
