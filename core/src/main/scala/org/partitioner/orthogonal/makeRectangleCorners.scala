c1b42ad7b05673f2c2f6f093c6bd5162a95a7740
Daniel <dan.jordon@gmail.com>
2017-07-16 19:16:00 -0400

    Move partition methods out into their own objects

---
+++ core/src/main/scala/org/partitioner/orthogonal/makeRectangleCorners.scala
@@ -0,0 +1,75 @@
+package org.partitioner.orthogonal
+
+import org.partitioner._
+
+
+object makeRectangleCorners extends Function1[List[List[Corner]], List[CornerGeometry]] {
+
+  import OrthogonalPolygonCornerExtender.extendCorners
+
+  private[partitioner] def createInteriorLines(
+      corners: List[List[Corner]]): List[CornerGeometry] = {
+
+    val co: List[Corner] = corners.flatMap(_.tail)
+
+    extendCorners(co)(true) ::: extendCorners(co)(false)
+  }
+
+  private[partitioner] def extractChordCorners(
+      chords: List[Chord],
+      vertical: Boolean): List[Corner] = {
+
+    chords
+      .filter(_.pointsVertically == vertical)
+      .flatMap(_.toListCorner)
+      .map(_.copy(isConcave = false))
+
+  }
+
+  private[partitioner] def separateChordsCornerLines(
+      interiorLines: List[CornerGeometry]): (List[Chord], List[CornerLine]) = {
+
+    val chords: List[Chord] = OrthogonalPolygonChordReducer.reduceChords {
+      interiorLines collect { case c: Chord => c }
+    }
+
+    val chordPoints: Set[Point] = chords
+      .flatMap(_.toListCorner)
+      .map(_.point)
+      .toSet
+
+    val lines: List[CornerLine] = interiorLines collect {
+      case ec: CornerLine if !chordPoints.contains(ec.source) => ec
+      case ch: Chord if !(chordPoints contains ch.point) => ch.toCornerLine
+    }
+
+    (chords, lines)
+  }
+
+  private[partitioner] def createChordsCornerLines: List[List[Corner]] => (List[Chord], List[CornerLine]) = {
+    (createInteriorLines _) andThen (separateChordsCornerLines _)
+  }
+
+  def apply(corners: List[List[Corner]]): List[CornerGeometry] = {
+
+    val (chords, lines) = createChordsCornerLines(corners)
+
+    val horizontalLines: List[CornerLine] = CornerLineAdjuster.adjustCornerGeometries {
+      lines.filterNot(_.pointsVertically) ::: extractChordCorners(chords, true)
+    } {
+      false
+    }
+
+    val verticalLines: List[CornerLine] = CornerLineAdjuster.adjustCornerGeometries {
+      horizontalLines.flatMap(_.toListCorner) :::
+        lines.filter(_.pointsVertically) :::
+        extractChordCorners(chords, false)
+    } {
+      true
+    }
+
+    val convexCorners = corners.flatMap(_.tail).filterNot(_.isConcave)
+
+    chords ::: verticalLines ::: horizontalLines ::: convexCorners
+  }
+}
